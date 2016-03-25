bc95df87a5dd62765a6b3bd882d29c97160fe4fb
jpk24rent <jarkko@24rent.fi>
2016-03-25 17:23:16 +0200

    first draft for Laby now with hearts

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name RecoverLaboratory.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the RecoverLaboratory
+ */
+angular.module('RecoverLaboratory')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name RecoverLaboratory.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the RecoverLaboratory
+ */
+angular.module('RecoverLaboratory')
+  .controller('MainCtrl', function () {
+
+  	$('.whoareyou').typed({
+    	strings: ["Who are you?"],
+	 	contentType: 'html', // or 'text'
+	 	startDelay: 1000,
+	});
+
+  	$('.friend').typed({
+    	strings: ["Friend?"],
+	 	contentType: 'html', // or 'text'
+	 	startDelay: 2000,
+	 	showCursor: false,
+	});
+  	$('.snake').typed({
+    	strings: ["a Snake?"],
+	 	contentType: 'html', // or 'text'
+	 	startDelay: 3000,
+	 	showCursor: false,
+	});
+  	$('.me').typed({
+    	strings: ["Me?"],
+	 	contentType: 'html', // or 'text'
+	 	startDelay: 4000,
+	 	showCursor: false,
+	});			
+
+  });
